7f6b33a9a909f02d7931d38a4a04f70d1f7c5e72
BBastien17 <106978157+BBastien17@users.noreply.github.com>
2023-08-01 13:41:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -199,4 +199,5 @@ if __name__ == '__main__':
     #app.run(host="20.105.232.31", port=8080)
     #app.run(host="0.0.0.0", port=8080)
     #app.run(host="0.0.0.0", port=5000)
-    app.run()
+    #app.run()
+    app.run(host='0.0.0.0', port=80, debug=True)
