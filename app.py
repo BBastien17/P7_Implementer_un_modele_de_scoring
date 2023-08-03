4bbf20d57072dcf5d23e6890b562c94ec3d100f6
BBastien17 <106978157+BBastien17@users.noreply.github.com>
2023-08-04 00:16:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return 'hello, world
+
+
+if __name__ == '__main__':
+    
+    port = os.environ.get("Port", 5000)
+    app.run(debug=False, host="0.0.0.0", port=port)
+    
